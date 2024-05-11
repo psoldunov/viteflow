06db3697e730b0a37006a7ee64378e6a3beb0727
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-05-11 23:54:40 +0300

    added scope plugin

--- src/config/vite.config.ts
+++ src/config/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+//@ts-ignore
+import scopeTailwind from "vite-plugin-scope-tailwind";
 import { resolve, join } from 'path';
 
 const processPath = process.cwd();
 
 export default defineConfig({
-	plugins: [cssInjectedByJsPlugin()],
+	plugins: [cssInjectedByJsPlugin(), scopeTailwind()],
 	build: {
 		rollupOptions: {
 			input: join(processPath, '/.viteflow/main.js'),
