efeb822c2c5c7034fafd48e0145be5834441001d
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-08-13 18:03:37 +0300

    commonjs plugin

--- src/config/vite.config.ts
+++ src/config/vite.config.ts
@@ -1,29 +1,30 @@
-import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
-import { resolve, join } from 'path';
+import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import commonjs from "vite-plugin-commonjs";
+import { resolve, join } from "path";
 
 const processPath = process.cwd();
 
 export default defineConfig({
-	plugins: [cssInjectedByJsPlugin()],
-	build: {
-		rollupOptions: {
-			input: join(processPath, '/.viteflow/main.js'),
-			output: {
-				entryFileNames: 'main.js',
-				assetFileNames: `[name].[ext]`,
-			},
-		},
-	},
-	resolve: {
-		extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.css', '.scss'],
-		alias: {
-			'@': resolve(processPath, './src'),
-			'@pages': resolve(processPath, './src/pages'),
-			'@components': resolve(processPath, './src/components'),
-			'@styles': resolve(processPath, './src/styles'),
-			'@plugins': resolve(processPath, './src/plugins'),
-			'@functions': resolve(processPath, './src/functions'),
-		}
-	},
+  plugins: [cssInjectedByJsPlugin(), commonjs()],
+  build: {
+    rollupOptions: {
+      input: join(processPath, "/.viteflow/main.js"),
+      output: {
+        entryFileNames: "main.js",
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".json", ".css", ".scss"],
+    alias: {
+      "@": resolve(processPath, "./src"),
+      "@pages": resolve(processPath, "./src/pages"),
+      "@components": resolve(processPath, "./src/components"),
+      "@styles": resolve(processPath, "./src/styles"),
+      "@plugins": resolve(processPath, "./src/plugins"),
+      "@functions": resolve(processPath, "./src/functions"),
+    },
+  },
 });
